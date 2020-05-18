934b9532fa7e1ce29f9514a7374152e3a397c839
Mohamed-Boukricha <48123922+Mohamed-Boukricha@users.noreply.github.com>
2020-05-18 01:43:16 +0000

    Update component-loader.js

--- scripts/component-loader.js
+++ scripts/component-loader.js
@@ -1,10 +1,3 @@
-  (function(){
-    var redirect = sessionStorage.redirect;
-    delete sessionStorage.redirect;
-    if (redirect && redirect != location.href) {
-      history.replaceState(null, null, redirect);
-    }
-  })();
 /**
  * Module ComponentLoader
  * Est appellé par ApplicationRouter (voir /scripts/application-router.js)
